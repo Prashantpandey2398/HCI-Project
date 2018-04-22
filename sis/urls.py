250bf3eeb410fef15c4bb70169196bc96592bea6
HITESHGOEL08 <hiteshgoel426@gmail.com>
2018-04-22 17:05:13 +0530

    hello

--- sis/urls.py
+++ sis/urls.py
@@ -4,4 +4,9 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^home',views.home1,name='home1'),
+    url(r'^marks',views.marks,name='marks'),
+    url(r'^dac',views.dac,name='dac'),
+    url(r'^feedback',views.feedback,name='feedback'),
+    url(r'^news',views.news,name='news'),
+    url(r'^login',views.afterlogin,name='afterlogin'),
 ]
--- sis/views.py
+++ sis/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
     context_dict = {}
-    return render(request,'sis/base.html',context_dict)
+    return render(request, 'sis/home.html', context_dict)
+
 
 def home1(request):
     context_dict = {}
-    return render(request,'sis/home.html',context_dict)
+    return render(request, 'sis/home.html', context_dict)
+
+
+def marks(request):
+    context_dict = {}
+    return render(request, 'sis/hitesh.html', context_dict)
+
+
+def dac(request):
+    context_dict = {}
+    return render(request, 'sis/akshat.html', context_dict)
+
+
+def feedback(request):
+    context_dict = {}
+    return render(request, 'sis/abhinav.html', context_dict)
+
+
+def news(request):
+    context_dict = {}
+    return render(request, 'sis/tushar.html', context_dict)
+
+
+def afterlogin(request):
+    context_dict = {}
+    return render(request, 'sis/sankalp.html', context_dict)
